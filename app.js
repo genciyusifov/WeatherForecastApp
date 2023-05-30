0c810ce7186255f0b94ffba59f867b0aaa887bb8
Gənci <yusifovgenci91@gmail.com>
2023-05-30 15:33:12 +0400

    Update

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const country = document.getElementById("country");
-const derece = document.getElementById("derece");
-const info = document.getElementById("info");
+const country = document.querySelector(".location");
+const derece = document.querySelector(".temperature")
+const info = document.querySelector(".description");
+const png = document.querySelector(".icon")
+const png2 = document.querySelector(".icon2")
 let data;
 
 const apiKey = "8c082e67fb0e269bb3f60c7919b4dc22";
@@ -12,15 +14,22 @@ function hava() {
   fetch(url)
     .then((response) => response.json())
     .then(function (data) {
+      // console.log(data);
       if (data.sys.country == undefined ) {
         country.innerHTML = "Respublika tapılmadı";
       } else if (data.name == undefined || data.sys.country == undefined) {
         country.innerHTML = "Tapılmadı";
+        png.innerHTML = ""
+        png2.innerHTML = ""
       } else {
         country.innerHTML = `${data.name}, ${data.sys.country}`;
-        derece.innerHTML = `max: ${Math.floor(data.main.temp_max) - 273}° / min: ${Math.floor(data.main.temp_min - 273)}°`;
+        png.innerHTML = `<img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="png"/>`
+        png2.innerHTML = `<img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="png"/>`
+        if(Math.floor(data.main.temp_max) - 273 == Math.floor(data.main.temp_min) - 273){
+          derece.innerHTML = ` ${Math.floor(data.main.temp_max) - 273}° `
+        }else derece.innerHTML = `min: ${Math.floor(data.main.temp_min) - 273}° / max: ${Math.floor(data.main.temp_max - 273)}°`;
         let x = data.weather[0].description;
-        info.innerHTML = x.charAt(0).toUpperCase() + x.slice(1);
+        info.innerHTML = x.charAt(0).toUpperCase() + x.slice(1); 
       }
     })
     .catch(function (error) {
