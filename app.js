e94b5245f1a4b61b8269e5c5edc949d7f51f35d0
Gənci <yusifovgenci91@gmail.com>
2023-06-01 14:29:07 +0400

    Update

--- app.js
+++ app.js
@@ -3,19 +3,26 @@ const derece = document.querySelector(".temperature")
 const info = document.querySelector(".description");
 const png = document.querySelector(".icon")
 const png2 = document.querySelector(".icon2")
+const button = document.querySelector("button")
 let data;
 
 const apiKey = "8c082e67fb0e269bb3f60c7919b4dc22";
 
 function hava() {
-  let city = document.getElementById("searchCity").value;
+  button.style.transition = ".2s"
+  button.style.scale = ".9"
+  let city = document.getElementById("searchCity").value.trim()
+  if (city === "") {
+    city = "Azerbaijan";
+  }
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=az&appid=${apiKey}`;
+
 
   fetch(url)
     .then((response) => response.json())
     .then(function (data) {
-      // console.log(data);
-      if (data.sys.country == undefined ) {
+      console.log(data.name);
+      if (data.sys.country == undefined) {
         country.innerHTML = "Respublika tapılmadı";
       } else if (data.name == undefined || data.sys.country == undefined) {
         country.innerHTML = "Tapılmadı";
@@ -25,19 +32,21 @@ function hava() {
         country.innerHTML = `${data.name}, ${data.sys.country}`;
         png.innerHTML = `<img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="png"/>`
         png2.innerHTML = `<img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="png"/>`
-        if(Math.floor(data.main.temp_max) - 273 == Math.floor(data.main.temp_min) - 273){
+        if (Math.ceil(data.main.temp_max) - 273 == Math.ceil(data.main.temp_min) - 273) {
           derece.innerHTML = ` ${Math.floor(data.main.temp_max) - 273}° `
-        }else derece.innerHTML = `min: ${Math.floor(data.main.temp_min) - 273}° / max: ${Math.floor(data.main.temp_max - 273)}°`;
+        } else derece.innerHTML = `min: ${Math.ceil(data.main.temp_min) - 273}° / max: ${Math.ceil(data.main.temp_max - 273)}°`;
         let x = data.weather[0].description;
-        info.innerHTML = x.charAt(0).toUpperCase() + x.slice(1); 
+        info.innerHTML = x.charAt(0).toUpperCase() + x.slice(1);
       }
     })
     .catch(function (error) {
-        console.log(error);
+      console.log(error);
       country.innerHTML = "Tapılmadı";
       derece.innerHTML = "";
       info.innerHTML = "";
     });
+
 }
+hava()
 
 // Ganji Yusifov
