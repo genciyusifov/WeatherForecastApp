2257adab6b2903a8879f04ca00a680d14e878e8e
Gənci <yusifovgenci91@gmail.com>
2023-06-01 14:43:48 +0400

    new

--- app.js
+++ app.js
@@ -22,9 +22,16 @@ function hava() {
     .then((response) => response.json())
     .then(function (data) {
       console.log(data.name);
-      if (data.sys.country == undefined ) {
-        country.innerHTML = data.name ;
-      } else if (data.name == undefined && data.sys.country == undefined) {
+      if (data.sys.country == undefined) {
+        country.innerHTML = data.name;
+        png.innerHTML = `<img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="png"/>`
+        png2.innerHTML = `<img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="png"/>`
+        if (Math.ceil(data.main.temp_max) - 273 == Math.ceil(data.main.temp_min) - 273) {
+          derece.innerHTML = ` ${Math.floor(data.main.temp_max) - 273}° `
+        } else derece.innerHTML = `min: ${Math.ceil(data.main.temp_min) - 273}° / max: ${Math.ceil(data.main.temp_max - 273)}°`;
+        let x = data.weather[0].description;
+        info.innerHTML = x.charAt(0).toUpperCase() + x.slice(1);
+      } else if (data.name == undefined || data.sys.country == undefined) {
         country.innerHTML = "Tapılmadı";
         png.innerHTML = ""
         png2.innerHTML = ""
